c4203b5da158313513a1665b79c3b724a9ce662a
asmarking <amarking@ucsd.edu>
2022-02-04 14:45:32 -0800

    fixed error again oops

--- SkillDemoOneTest.java
+++ SkillDemoOneTest.java
@@ -10,10 +10,10 @@ java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUn
 
 */
 
-public class SkillDemonstrationTest {
+public class SkillDemoOneTest {
     @Test
     public void subtractTest() {
-        assertEquals(2,SkillDemonstration.subtract(4,2)) ;
+        assertEquals(2,SkillDemoOne.subtract(4,2)) ;
     }
 }
 
